dba31965290b4c877dad181009652c1a015bce24
Tim Krapil <tim.krapil@channeladvisor.com>
2013-07-08 16:08:35 -0400

    first test

---
+++ test2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tim.krapil
+ * Date: 7/8/13
+ * Time: 4:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$redis = new Redis();
+
+$redis->connect('127.0.0.1', 6379);
+$info = $redis->info();
+echo $info;
