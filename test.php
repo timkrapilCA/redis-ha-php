6dff09bc7cd0eaba0bf47965e3cdb15483bfe917
Tim Krapil <tim.krapil@channeladvisor.com>
2013-07-08 14:01:55 -0400

    first test

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tim.krapil
+ * Date: 7/8/13
+ * Time: 2:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+require 'Predis/Autoloader.php';
+
+Predis\Autoloader::register();
