191e90c7d4033a43a51dfd6269a1022cc985c27e
Tim Krapil <tim.krapil@channeladvisor.com>
2013-07-08 22:06:34 -0400

    figuring out arrays

--- test2.php
+++ test2.php
@@ -10,7 +10,7 @@
 $redis = new Redis();
 
 $redis->connect('127.0.0.1', 6379);
-$info = array($redis->info());
+$info = $redis->info();
 foreach ($info as $value) {
-    echo $value; '<br>';
+    echo $value; "<br>";
 }
